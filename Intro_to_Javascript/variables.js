32cb4af96648c074cac6675aa27b1d3b1d22d12b
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-08-13 21:12:32 -0700

    Learning about javascript vatiables

---
+++ Intro_to_Javascript/variables.js
@@ -0,0 +1,27 @@
+/*
+Create a Variable: var
+...
+*/
+var favoriteFood = "pizza";
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+//Output: pizza \n 8
+   
+/*
+Create a Variable: let
+...
+*/
+let changeMe = true;
+changeMe = false;
+console.log(changeMe)
+//Output: false
+
+/*
+Create a Variable: const
+...
+*/
+const entree = 'Enchiladas';
+console.log(entree);
+entree = "Tacos";
+//Output: Enchiladas \n TypeError: Assignment to constant variable.
