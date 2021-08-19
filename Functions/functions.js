e217c956b869f2842afa58b083d1dab630f327be
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-08-19 16:25:25 -0700

    commenting on the use of functions in javascript

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,6 +1,6 @@
 /*
 Function Declarations
-...
+A function is a reusable block of code that groups together a sequence of statements to perform a specific task.
 */
 function getReminder(){
 	console.log('Water the plants.');
@@ -12,7 +12,7 @@ function greetInSpanish(){
 
 /*
 Calling a Function
-
+Write the name of the function, and the paratheses and optional parameters to invoke the the body of the of the function.
 */
 function sayThanks(){
 	console.log('Thank you for your purchase! We appreciate your business.');
@@ -24,7 +24,7 @@ sayThanks();
 
 /*
 Parameters and Arguments
-...
+A parameter is a named variable inside a function’s block which will be assigned the value of the argument passed in when the function is invoked:
 */
 function sayThanks(name) {
 	console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
@@ -34,7 +34,7 @@ sayThanks('Cole');
 
 /*
 Default Parameters
-...
+ES6 introduces new ways of handling arbitrary parameters through default parameters which allow us to assign a default value to a parameter in case no argument is passed into the function.
 */
 function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
 	console.log(`Remember to buy ${item1}`);
@@ -44,7 +44,7 @@ function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
       
 /*
 Return
-...
+To return a value from a function, we use a return statement.
 */
 function monitorCount(rows,columns){
 	return rows * columns;
@@ -56,7 +56,7 @@ console.log(numOfMonitors);
 
 /*
 Helper Functions
-...
+Smaller functions which help bigger functions
 */
 function costOfMonitors(rows, columns){
 	return monitorCount(rows, columns) * 200;
@@ -94,6 +94,6 @@ const plantNeedsWater = (day) => {
 
 /*
 Concise Body Arrow Functions
-...
+Function definition can be made concise using concise arrow notation:
 */
 const plantNeedsWater = day => day === 'Wednesday' ? true : false;
