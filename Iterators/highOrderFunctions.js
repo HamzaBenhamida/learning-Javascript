60fd9867b4679356471d9e514779a40829ca6058
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-08-23 13:15:34 -0700

    understanding how high order function work in js

--- Iterators/highOrderFunctions.js
+++ Iterators/highOrderFunctions.js
@@ -2,3 +2,19 @@
 Functions as Data
 ...
 */
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+	for(let i = 1; i <= 1000000; i++) {
+	  if ( (2 + 2) != 4) {
+	    console.log('Something has gone very wrong :( ');
+	  }
+	}
+      }
+      
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name) // prints the original name of the function
+
+/*
+Functions as Parameters
+...
+*/
