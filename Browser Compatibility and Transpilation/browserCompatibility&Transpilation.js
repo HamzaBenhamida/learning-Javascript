27ce451bb0e366c0e50dd55478db12e818c5a42c
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-09-02 12:51:14 -0700

    starting to learn browser compatibility

---
+++ Browser Compatibility and Transpilation/browserCompatibility&Transpilation.js
@@ -0,0 +1,25 @@
+/*
+Introduction
+...
+*/
+var pasta = "Spaghetti"; // ES5 syntax
+ 
+const meat = "Pancetta"; // ES6 syntax
+ 
+let sauce = "Eggs and cheese"; // ES6 syntax
+ 
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+/*
+caniuse.com I
+we will use caniuse.com to find the percentage of browsers that support the entire ES5 library, 
+then we will look at the percentage of browsers that support modules, a feature introduced in ES6.
+*/
+
+/*
+caniuse.com II
+...
+*/
+let esFivePercentageSupport = 99.71;
+let esSixTemplateLiterals = 98.11;
