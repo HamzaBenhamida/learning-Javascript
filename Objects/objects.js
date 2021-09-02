aad54b41c37d2c59b4393d461a1482d3f2152b00
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-09-02 09:12:04 -0700

    starting to learn how objects work in javascript

---
+++ Objects/objects.js
@@ -0,0 +1,29 @@
+/*
+Creating Object Literals
+...
+*/
+const fasterShip = {
+	'Fuel Type': 'Turbo Fuel',
+	color: 'silver'
+      };
+
+/*
+Accessing Properties
+..
+*/
+let spaceship = {
+	homePlanet: 'Earth',
+	color: 'silver',
+	'Fuel Type': 'Turbo Fuel',
+	numCrew: 5,
+	flightPath: ['Venus', 'Mars', 'Saturn']
+      };
+      
+let crewCount = spaceship.numCrew;
+      
+const planetArray = spaceship.flightPath;
+
+/*
+Bracket Notation 
+...
+*/
